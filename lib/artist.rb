7399cd3d0d6988437625336aa4ddcf32f2098c36
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-06-16 03:36:32 +0300

    completed assignment

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,10 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
+    extend Memorable::ClassMethods
+    extend Findable::ClassMethods
+
+
   @@artists = []
 
   def initialize
@@ -19,13 +23,7 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
+  
 
   def add_song(song)
     @songs << song
